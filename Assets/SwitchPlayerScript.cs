d0ccc9f6a6faadb60c6d314d509c160ce2ede2cd
Tjieco van Donselaar <T.vanDonselaar@student.han.nl>
2019-03-25 15:02:01 +0100

    Player switch implementation

---
+++ Assets/SwitchPlayerScript.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class SwitchPlayerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Player player;
+    public Mesh placeholderMesh;
+
+    private SphereCollider headCollider;
+
+    void Start()
+    {
+        headCollider = (SphereCollider) GetComponent<Player>().headCollider;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Switch"))
+        {
+            player.gameObject.SetActive(true);
+
+            Destroy(GameObject.Find("Placeholder"));
+            CreateNewPlaceholder();
+
+            gameObject.SetActive(false);
+        }
+    }
+
+    void CreateNewPlaceholder()
+    {
+        var placeholderGameObject = new GameObject("Placeholder");
+        var meshFilter = placeholderGameObject.AddComponent<MeshFilter>();
+        placeholderGameObject.AddComponent<MeshRenderer>();
+        meshFilter.sharedMesh = placeholderMesh;
+        placeholderGameObject.transform.position = headCollider.transform.position;
+        placeholderGameObject.transform.rotation = headCollider.transform.rotation;
+    }
+}
