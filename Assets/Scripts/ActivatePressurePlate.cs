4ca2d4b09b00130bcf278f5f9c70afb7844ea65f
Tim Hendriksen <tim_hendriksen@outlook.com>
2019-03-25 13:53:01 +0100

    Changed collider on plate to interact with the head of the player. Now you dont need to change the player.

--- Assets/Scripts/ActivatePressurePlate.cs
+++ Assets/Scripts/ActivatePressurePlate.cs
@@ -1,13 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+using Valve.VR.InteractionSystem;
 
 public class ActivatePressurePlate : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.SetActive(false);   
+        gameObject.SetActive(true);   
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+//        if (other.CompareTag("Player"))
+//        {
+            transform.position = new Vector3( transform.position.x ,transform.position.y - 0.05f, transform.position.z);
+//        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+//        if (other.CompareTag("Player"))
+//        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.05f, transform.position.z);
+//        }
     }
 
     public void ActivatePressurePlateFunction() {
