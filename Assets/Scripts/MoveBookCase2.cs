813a9691b45c30ea3af631d59d7232240c221acf
Tim Hendriksen <tim_hendriksen@outlook.com>
2019-03-25 15:09:11 +0100

    Added file to move bookcase

---
+++ Assets/Scripts/MoveBookCase2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class MoveBookCase2 : MonoBehaviour
+{
+    public float MoveBy = 0.1f;
+    public int MoveNumberOfTimes = 10;
+    void OnEnable()
+    {
+        EventManager.StartListening("MoveBookCase", SomeFunction);
+    }
+
+    void SomeFunction()
+    {
+        StartCoroutine("MoveBookCase");
+    }
+
+    IEnumerator MoveBookCase()
+    {
+        float timePassed = 0;
+        while (timePassed < MoveNumberOfTimes)
+        {
+            transform.Translate(new Vector3(transform.position.x - MoveBy, transform.position.y, transform.position.z));
+            timePassed += Time.deltaTime;
+
+            yield return null;
+        }
+    }
+}
