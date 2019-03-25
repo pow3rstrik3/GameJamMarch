eb49627b3fe943d33b29321d3ba6d59154931d85
Don Duijnstee <donduijnstee@hotmail.nl>
2019-03-25 15:18:24 +0100

    Added conversation between jeff and student

---
+++ Assets/StatePattern/IdleState.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : IStudentState
+{
+    public IdleState(StudentController aStudent) : base(aStudent) { }
+
+    public override void onEntry()
+    {
+        student.setState(new TalkingState(student));
+    }
+
+    public override void onExit()
+    {
+
+    }
+
+    public override void onFixedUpdate()
+    {
+
+    }
+
+    public override void onUpdate()
+    {
+
+    }
+}
