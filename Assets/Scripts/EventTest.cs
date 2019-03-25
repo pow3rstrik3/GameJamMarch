67b89bb0266b8e007659af9804b5d9ca33cfc3e5
Bas van de Louw <B.vandeLouw@student.han.nl>
2019-03-25 12:34:58 +0100

    added event manager

---
+++ Assets/Scripts/EventTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class EventTest : MonoBehaviour {
+
+      void OnEnable ()
+    {
+        EventManager.StartListening ("Light", TurnOnLights);
+    }
+
+    void OnDisable ()
+    {
+        EventManager.StopListening ("Light", TurnOnLights);
+    }
+
+    void TurnOnLights ()
+    {
+        Debug.Log ("The lights turned on");
+    }
+}
---
+++ Assets/Scripts/EventTriggerTest.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventTriggerTest : MonoBehaviour {
+
+
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            EventManager.TriggerEvent ("Light");
+        }
+    }
+}
